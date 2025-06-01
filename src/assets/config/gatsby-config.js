234acce91f0b6cff5797d8c2eded32a89f5ce7f6
Volodymyr Vorobiov <funnyshotgun@gmail.com>
2025-06-01 10:36:52 +0200

    добавил robots.txt и sitemap

--- src/assets/config/gatsby-config.js
+++ src/assets/config/gatsby-config.js
@@ -1,22 +1,28 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://lavandi.pl", // укажи точный адрес сайта
+    siteUrl: "https://lavandi.pl",
     title: "Lavandi – Studio masażu w Warszawie",
     description:
       "Lavandi oferuje profesjonalne masaże w Warszawie: relaksacyjny, klasyczny, limfodrenaż, sportowy, antycellulitowy, twarzy oraz masaż autorski. Umów się online i poczuj ulgę i odprężenie.",
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-image`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        output: `/sitemap.xml`,
+      },
+    },
     {
       resolve: `gatsby-plugin-robots-txt`,
       options: {
+        host: "https://lavandi.pl",
+        sitemap: "https://lavandi.pl/sitemap.xml",
         policy: [{userAgent: "*", allow: "/"}],
       },
     },
-    // Если используешь изображения:
-    `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-image`,
   ],
 };
