6aa01e097063beab0afb8df87aaeede60eb6a1ee
Volodymyr Vorobiov <funnyshotgun@gmail.com>
2026-07-21 12:26:30 +0200

    style(home): set hero banner height to 112vh

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,7 +2,7 @@ import HeroCarousel from "./HeroCarousel";
 
 function Home() {
   return (
-    <section id="home" className="relative z-[1] h-[98vh] min-h-[740px] max-h-[1000px] w-full overflow-hidden">
+    <section id="home" className="relative z-[1] h-[112vh] min-h-[920px] max-h-[1180px] w-full overflow-hidden">
       <HeroCarousel />
     </section>
   );
